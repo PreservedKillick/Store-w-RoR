a6f477e76fae808e07aa7b531d5fa60e7d152ff7
Maureen Dugan & Angela Yen <student@epicodus.com>
2014-04-11 10:22:42 -0700

    create class validations

---
+++ app/models/product.rb
@@ -0,0 +1,9 @@
+class Product < ActiveRecord::Base
+
+  validates :name, :presence => true
+  validates :price, :presence => true
+  validates :description, :presence => true
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+end
